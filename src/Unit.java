3cb59b638b41d238d9411e539f8ce47e345679bf
Leonid <airspacece@mail.ru>
2023-02-19 09:47:26 +1000

    123

--- src/Unit.java
+++ src/Unit.java
@@ -8,7 +8,6 @@ public abstract class Unit<T> extends GameObject implements Interaction<T> {
     protected int health;
     protected int damage;
     protected int speed;
-    protected int maxSpeed;
 
     public Unit() {
         super();
